0fcf585c730f751574cffc60b9d14d27421653ad
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-10-01 00:40:18 -0300

    feat: create card module and swagger-doc

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './modules/task/task.module';
 import { FavoriteModule } from './modules/favorite/favorite.module';
+import { CardsModule } from './modules/cards/cards.module';
 
 @Module({
-  imports: [TaskModule, FavoriteModule],
+  imports: [TaskModule, FavoriteModule, CardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
